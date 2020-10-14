526d918f75f05f95b780f1d2ef0e918667629821
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-10-14 15:17:32 -0500

    done omg done

--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,19 @@
 // fill out this container
+import { connect } from "react-redux";
+import { fetchMakes, deleteMake } from "../redux/actions";
+import Import from "./../components/Import";
+
+const mapStateToProps = (state) => {
+  return {
+    makes: state.makes,
+  };
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchMakes: () => dispatch(fetchMakes()),
+    deleteMake: (index) => dispatch(deleteMake(index)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import);
